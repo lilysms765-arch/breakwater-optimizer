91a1f86fe347bca3718040feb23cf193f104ec76
lilysms765-arch <lilysms765@gmail.com>
2026-06-08 23:47:06 +0530

    Update HudsonCalculator.java

--- src/main/java/com/breakwater/physics/HudsonCalculator.java
+++ src/main/java/com/breakwater/physics/HudsonCalculator.java
@@ -1,1 +1,19 @@
+package com.breakwater.physics;
 
+public class HudsonCalculator {
+
+    private static final double KD = 8.0;
+    private static final double SR = 2.4;
+
+    public static double calculateArmorWeight(
+            double waveHeight,
+            double slope) {
+
+        double numerator = Math.pow(waveHeight, 3);
+
+        double denominator =
+                KD * Math.pow(SR - 1, 3) * slope;
+
+        return numerator / denominator;
+    }
+}
