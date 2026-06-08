42e6880e2fd957d01643f9c76ceda2687d2ef9ba
lilysms765-arch <lilysms765@gmail.com>
2026-06-08 23:46:29 +0530

    Update EnvironmentData.java

--- src/main/java/com/breakwater/model/EnvironmentData.java
+++ src/main/java/com/breakwater/model/EnvironmentData.java
@@ -1,1 +1,28 @@
+package com.breakwater.model;
 
+public class EnvironmentData {
+
+    private double waveHeight;
+    private double waterDepth;
+
+    public EnvironmentData(double waveHeight, double waterDepth) {
+        this.waveHeight = waveHeight;
+        this.waterDepth = waterDepth;
+    }
+
+    public double getWaveHeight() {
+        return waveHeight;
+    }
+
+    public double getWaterDepth() {
+        return waterDepth;
+    }
+
+    public void setWaveHeight(double waveHeight) {
+        this.waveHeight = waveHeight;
+    }
+
+    public void setWaterDepth(double waterDepth) {
+        this.waterDepth = waterDepth;
+    }
+}
