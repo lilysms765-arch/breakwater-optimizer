88045662d296d767d499b6016036372e027102c6
lilysms765-arch <lilysms765@gmail.com>
2026-06-08 23:46:47 +0530

    Update BreakwaterDesign.java

--- src/main/java/com/breakwater/model/BreakwaterDesign.java
+++ src/main/java/com/breakwater/model/BreakwaterDesign.java
@@ -1,1 +1,37 @@
+package com.breakwater.model;
 
+public class BreakwaterDesign {
+
+    private double slope;
+    private double armorWeight;
+    private double concreteVolume;
+    private boolean craneFeasible;
+
+    public BreakwaterDesign(
+            double slope,
+            double armorWeight,
+            double concreteVolume,
+            boolean craneFeasible) {
+
+        this.slope = slope;
+        this.armorWeight = armorWeight;
+        this.concreteVolume = concreteVolume;
+        this.craneFeasible = craneFeasible;
+    }
+
+    public double getSlope() {
+        return slope;
+    }
+
+    public double getArmorWeight() {
+        return armorWeight;
+    }
+
+    public double getConcreteVolume() {
+        return concreteVolume;
+    }
+
+    public boolean isCraneFeasible() {
+        return craneFeasible;
+    }
+}
